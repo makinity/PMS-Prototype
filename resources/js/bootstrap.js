8f5e81e4d69151eb34e7a6b9fe7432cf8fe38248
makinity <marklionesios@gmail.com>
2026-05-04 15:36:47 +0800

    prepared notification feature

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,5 +23,3 @@ window.Echo = new Echo({
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allow your team to quickly build robust real-time web applications.
  */
-
-import './echo';
